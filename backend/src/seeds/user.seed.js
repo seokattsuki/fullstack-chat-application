20cd17d64f504f447d136182d01e8664433b3d5a
rohittpatra <info.rohitpatra@gmail.com>
2025-07-26 13:53:29 +0530

    project complete

---
+++ backend/src/seeds/user.seed.js
@@ -0,0 +1,30 @@
+
+import { config } from "dotenv";
+import { connectDB } from "../lib/db.js";
+import User from "../models/user.model.js";
+
+config();
+
+const seedUsers = [
+  // Female Users
+  {
+    email: "daniel.rodriguez@example.com",
+    fullName: "Daniel Rodriguez",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/men/7.jpg",
+  },
+];
+
+const seedDatabase = async () => {
+  try {
+    await connectDB();
+
+    await User.insertMany(seedUsers);
+    console.log("Database seeded successfully");
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  }
+};
+
+// Call the function
+seedDatabase();
---
+++ frontend/src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage"
+import Navbar from './components/Navbar'; 
+import { useAuthStore } from './store/useAuthStore.js';
+import {Loader} from "lucide-react";
+import { Toaster } from "react-hot-toast";
+import {useThemeStore} from "./store/useThemeStore.js"
+
+const App = () => {
+  const {authUser, checkAuth, isCheckingAuth, onlineUsers} = useAuthStore();
+  const {theme} = useThemeStore()
+
+  console.log(onlineUsers);
+  
+  useEffect(() =>{
+    checkAuth()
+  }, [checkAuth]);
+
+  console.log({authUser});
+
+  if(isCheckingAuth && !authUser) return (
+    <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+  )
+
+  return (
+    <div data-theme={theme}>
+      <Navbar/>
+
+      <Routes>
+        <Route path="/" element={authUser? <HomePage/> : <Navigate to = "/login" />} />
+        <Route path="/signup" element={!authUser? <SignUpPage/> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser? <LoginPage/> : <Navigate to="/" />} />
+        {/* Fixed: Protect settings route */}
+        <Route path="/settings" element={authUser ? <SettingsPage/> : <Navigate to="/login" />} />
+        <Route path="/profile" element={authUser? <ProfilePage/> : <Navigate to = "/login" />} />
+      </Routes>
+
+      <Toaster/>
+    </div>
+  )
+}
+
+export default App
