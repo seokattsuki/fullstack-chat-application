af78580e4c1f7dcd29aacc7044f055f3b29e8cff
rohittpatra <info.rohittpatra@gmail.com>
2025-07-27 01:30:32 +0530

    Fix server crash due to path-to-regexp error and update route handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import authRoutes from "./routes/auth.routes.js";
-import messageRoutes from "./routes/message.routes.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import dotenv from 'dotenv';
 import { connectDB } from './lib/db.js';
 import cookieParser from "cookie-parser";
